fd0ce0266f76d447a1ee5d10af4454ae25cc82e3
suhaas02 <vsuhaas02@gmail.com>
2022-01-21 16:09:47 +0530

    updated ui

--- public/script.js
+++ public/script.js
@@ -1,19 +1,56 @@
+const socket = io('/');
 
-
+//streaming our own video
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
-
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+});
 let myVideoStream;
-//for accessing the camera and microphone.
+//for accessing the camera and microphone from browser.
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+
+        peer.on('call', call => {
+            call.answer(stream)
+            const video = document.createElement('video')
+            call.on('stream', userVideoStream => {
+                addVideoStream(video, userVideoStream)
+            })
+        })
+    })
+    
 })
 
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID);
+})
+
+
+const connectToNewUser = (userId) => {
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream',userVideoStream => {
+        addVideoStream(video,userVideoStream)
+    })
+}
+
+// const addVideoStream = (video, stream) => {
+//     video.srcObject = stream;
+//     video.addEventListener('loadedmetadata', () => {
+//         video.play();
+//     })
+//     videoGrid.append(video)
+// }
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
